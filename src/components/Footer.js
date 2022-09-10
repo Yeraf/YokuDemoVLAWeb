30a4d46e9f3acdcd19280d0225e4147be99df43f
Yeraf <yerafarceguerrero9@gmail.com>
2022-09-09 19:40:14 -0600

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,36 @@
 import React from "react";
-import '../CSS/Footer.css';
-import Copa from '../img/Images/Copa Airlines.png';
-import CableOnda from '../img/Images/Cable-onda.png';
-import ROCCA from '../img/Images/Rocca.png';
-import Auxios from '../img/Images/Auxix-logo.jpg';
-import Concentrix from '../img/Images/concentrix.jpg';
-import Fujitsu from '../img/Images/logo-fujitsu.png';
-
+import "../CSS/Footer.css";
+import Copa from "../img/Images/Copa Airlines.png";
+import CableOnda from "../img/Images/Cable-onda.png";
+import ROCCA from "../img/Images/Rocca.png";
+import Auxios from "../img/Images/Auxix-logo.jpg";
+import Concentrix from "../img/Images/concentrix.jpg";
+import Fujitsu from "../img/Images/logo-fujitsu.png";
+import CR from "../img/Flag/costa-rica.png";
+import Panama from "../img/Flag/panama.png";
+import Spain from "../img/Flag/spain.png";
+import Usa from "../img/Flag/united-states.png";
+import Face from "../img/Icons/facebook.png";
+import Instagram from "../img/Icons/instagram1.png";
+import Linkedin from "../img/Icons/linkedin.png";
+import Tiktok from "../img/Icons/tik-tok.png";
 
 function Footer() {
   return (
     <div className="Footer-main">
       <div className="footer-taxt">
-      <div className="footer-contact">
-          
-      </div>
-        <h5 className="footer-h5">Copañías que confían  en VLA</h5>
+        <h5 className="footer-h5">Contactos</h5>
+        <div className="footer-contact">
+          <img className="img-flag" src={CR}></img>
+          <p className="footer-p-numbers">+507 833-5602</p>
+          <img className="img-flag" src={Panama}></img>
+          <p className="footer-p-numbers">+507 833-5602</p>
+          <img className="img-flag" src={Spain}></img>
+          <p className="footer-p-numbers">+507 833-5602</p>
+          <img className="img-flag" src={Usa}></img>
+          <p className="footer-p-numbers">+507 833-5602</p>
+        </div>
+        <h5 className="footer-h5-companias">Copañías que confían en VLA</h5>
         <div className="footer-img">
           <img className="img-footer" src={Copa}></img>
           <img className="img-footer" src={CableOnda}></img>
@@ -24,8 +39,23 @@ function Footer() {
           <img className="img-footer" src={Concentrix}></img>
           <img className="img-footer" src={Fujitsu}></img>
         </div>
-        <p className="footer-p">Developep by &copy;YokuStudios 2022</p>
-        <p className="footer-p">studiosyoku@gmail.com</p>
+        <div className="footer-media">
+          <a className="footer-a-media">
+            <img className="footer-img-media" src={Face}></img>
+          </a>
+          <a className="footer-a-media">
+            <img className="footer-img-media" src={Instagram}></img>
+          </a>
+          <a className="footer-a-media">
+            <img className="footer-img-media" src={Tiktok}></img>
+          </a>
+          <a className="footer-a-media">
+            <img className="footer-img-media" src={Linkedin}></img>
+          </a>
+        </div>
+        <p className="footer-p">
+          Developep by &copy;YokuStudios 2022 / studiosyoku@gmail.com
+        </p>
       </div>
     </div>
   );

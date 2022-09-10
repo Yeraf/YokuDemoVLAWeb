4c902857f6ad2bf825eb28813c6f2403fac67e7e
Yeraf <yerafarceguerrero9@gmail.com>
2022-09-09 18:25:36 -0600

    commit flag

--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import Contacto from "./components/Contacto";
 import Empleados from "./components/Empleados";
 import Cursos from "./components/Cursos";
 import Footer from "./components/Footer";
+import Empresarial from "./components/Empresarial";
 
 function Router() {
   return (
@@ -15,10 +16,11 @@ function Router() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" exact element={<Main />}></Route>
-        <Route part="/acerca" element={<Acerca />}></Route>
-        <Route part="/contacto" element={<Contacto />}></Route>
-        <Route part="/empleados" element={<Empleados />}></Route>
-        <Route part="/cursos" element={<Cursos />}></Route>
+        <Route path="/acerca" element={<Acerca />}></Route>
+        <Route path="/contacto" element={<Contacto />}></Route>
+        <Route path="/empleados" element={<Empleados />}></Route>
+        <Route path="/cursos" element={<Cursos />}></Route>
+        <Route path="/empresarial" element={<Empresarial />}></Route>
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
---
+++ src/components/Empresarial.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Empresarial() {
+  return (
+    <div>Empresarial</div>
+  )
+}
+
+export default Empresarial

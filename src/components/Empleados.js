9eb7cc895d8ded06b1d0506afdb2a4351e207780
Yeraf <yerafarceguerrero9@gmail.com>
2022-09-16 20:36:38 -0600

    carousel team

--- src/components/Empleados.js
+++ src/components/Empleados.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../CSS/Team.css";
 import "../CSS/swiper-bundle.min.css";
-import "../JS/swiper-bundle.min.js";
 import User1 from "../img/ImagesTeam/user1.jpg";
 import User2 from "../img/ImagesTeam/user2.jpg";
 import User3 from "../img/ImagesTeam/user3.jpeg";
@@ -23,25 +22,95 @@ function Empleados() {
             asignado, estamos para servirle.{" "}
           </p>
         </div>
-        <div className="slide-container">
-            <div className="slide-content">
-              <div className="card-wrapper">
-                <div className="card">
-                  <div className="image-content">
-                    <span className="overlay"></span>
-                    <div className="card-image">
-                      <img src={User1} alt="" className="card-img"></img>
+      </article>
+      <div className="contenedor-team-cards">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area"><img src={User1} alt=""></img></div>
+                        <div className="img-text">
+                          <h2>Person one</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area">
+                          <img src={User2} alt=""></img>
+                        </div>
+                        <div className="img-text">
+                          <h2>Person Two</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area">
+                          <img src={User3} alt=""></img>
+                        </div>
+                        <div className="img-text">
+                          <h2>Person Three</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="card-content">
-                    <h2 className="name">Nahuel Soprano</h2>
-                    <p className="description">asoprano@grupovla.com</p>
+                </div>
+                <div class="carousel-item">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area">
+                          <img src={User4} alt=""></img>
+                        </div>
+                        <div className="img-text">
+                          <h2>Person Four</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area">
+                          <img src={User2} alt=""></img>
+                        </div>
+                        <div className="img-text">
+                          <h2>Person Five</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="single-box">
+                        <div className="img-area">
+                          <img src={User1} alt=""></img>
+                        </div>
+                        <div className="img-text">
+                          <h2>Person Six</h2>
+                          <p>person@onegmail.com</p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
         </div>
-      </article>
+      </div>
+      </div>
     </div>
   );
 }
